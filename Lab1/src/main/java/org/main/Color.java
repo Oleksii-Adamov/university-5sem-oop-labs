13aebe3fe2c43197f9949991ad474c1af2db06b5
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-10-30 20:55:11 +0200

    started Lab1: defined Flower class

---
+++ Lab1/src/main/java/org/main/Color.java
@@ -0,0 +1,7 @@
+package org.main;
+
+public enum Color {
+    RED,
+    BLUE,
+    YELLOW
+}
---
+++ Lab1/src/main/java/org/main/Flower.java
@@ -0,0 +1,10 @@
+package org.main;
+
+public class Flower {
+    private String speciesName;
+    private Color color;
+    private int daysAfterGathering; // freshness
+    private int stemLen;
+    private int petalLen;
+    private int cost; // у копійках
+}
